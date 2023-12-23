779b62e05c023789173ed5a9612eab415080fb61
Mohammed Rerhaye <Mr.rghay@gmail.com>
2023-12-23 16:33:30 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 
 import MovieCard from "./movieCard";
-import SearchIcon from "./Search.svg"
+import SearchIcon from "./Search.svg";
 import "./App.css";
 
-const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
+const API_URL = "https://www.omdbapi.com?apikey=b6003d8a";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
